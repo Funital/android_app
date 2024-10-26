e1c2693d75b1c4396744cee977c8bb4adb8c2cdf
Funital <gksrmawns0925@naver.com>
2024-10-26 21:07:39 +0900

    [noobi/week2] 최종

---
+++ week2/RaMi/app/src/main/java/com/example/rami/TextProcessor.java
@@ -0,0 +1,15 @@
+package com.example.rami;
+
+/**
+ * 텍스트 처리를 위한 클래스
+ */
+public class TextProcessor {
+    /**
+     * 텍스트 하나를 입력받아 적절한 작업을 수행해 return합니다.
+     * @param str
+     * @return
+     */
+    public static String Processor(String str){
+        return str + "?";
+    }
+}
